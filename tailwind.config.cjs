865c7c0f7afcca9b0d2133879efbb3687f884ac5
kostya-alch <kostafrompiter@gmail.com>
2022-09-28 00:31:05 +0300

    tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,svelte,scss,css}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

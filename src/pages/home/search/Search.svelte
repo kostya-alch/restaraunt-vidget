9f51600a0a8d179bfb49f126bc474dc0b25346c6
kostya-alch <kostafrompiter@gmail.com>
2022-09-28 19:32:53 +0300

    search

--- src/pages/home/search/Search.svelte
+++ src/pages/home/search/Search.svelte
@@ -1,3 +1,31 @@
+<script lang="ts">
+  import IoIosSearch from 'svelte-icons/io/IoIosSearch.svelte';
+  let searchTerm = '';
+</script>
+
 <div class="search">
-  <input type="search" placeholder="Search.." />
+  <div class="icon">
+    <IoIosSearch />
+  </div>
+  <input type="search" placeholder="Search.." bind:value={searchTerm} />
 </div>
+
+<style lang="scss">
+  .search {
+    @apply overflow-hidden bg-[#F0F0F0] rounded-3xl py-2 px-3.5 flex flex-row;
+
+    .icon {
+      @apply w-7 mr-2 text-[#BCBCC1];
+    }
+
+    input {
+      @apply bg-transparent text-[#222];
+      border: none;
+      outline: none;
+
+      &::placeholder {
+        @apply font-light placeholder-stone-100;
+      }
+    }
+  }
+</style>
